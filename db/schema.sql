2e0dac4291f30112868bb0c317f3c93a4368a913
Meghan Mullally <ms.mullally27@gmail.com>
2019-07-11 15:19:43 -0700

    fixed brackets in schema table, added code to server.js express, port, routes

--- db/schema.sql
+++ db/schema.sql
@@ -2,12 +2,13 @@ CREATE DATABASE 'burgers_db';
 
 USE 'burgers_db';
 
-CREATE TABLE burgers {
+CREATE TABLE burgers (
   id INT AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR(50) NOT NULL,
-  devoured BOOLEAN NOT NULL,
+  devoured BOOLEAN,
   PRIMARY KEY (id)
-};
+);
+ 
 
 
 
