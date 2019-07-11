84bbb0491fb1f718c1257325a5c37880df4a5097
Meghan Mullally <ms.mullally27@gmail.com>
2019-07-11 16:47:22 -0700

    added data into seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheese Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Pulled Pork Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Avocado Bacon Cheese Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Teriyaki Burger");
